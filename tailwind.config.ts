011b95ed0e2069c3222e9a691ad485bf73c04840
heense <hrmadsen89@gmail.com>
2025-02-14 12:57:23 +0100

    chore: prepare for tailwind v4 upgrade

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,24 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      containers: {
+        xs: "20rem",
+        sm: "24rem",
+        md: "28rem",
+        lg: "32rem",
+        xl: "36rem",
+        "2xl": "42rem",
+        "3xl": "48rem",
+        "4xl": "56rem",
+        "5xl": "64rem",
+        "6xl": "72rem",
+        "7xl": "80rem",
+      },
     },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
   },
   plugins: [],
 } satisfies Config;
+
